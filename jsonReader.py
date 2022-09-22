cd63533da428fce12850a13fca0e6177d30dec9b
Shashank Beri <57190179+shashankberi@users.noreply.github.com>
2022-09-22 01:06:43 -0700

    Add files via upload
    Edits to make code more concise

--- jsonReader.py
+++ jsonReader.py
@@ -1,13 +1,9 @@
 import json
 import csv
 import argparse
-import logging
 import os
-from os import path
-from sys import argv
 from types import *
 from xml.dom.minicompat import StringTypes
-
 
 
 def main():
@@ -41,9 +37,11 @@ def main():
     try:
         # load all csv files in the inputFolder into an array of dictionaries
         for file in sorted(os.listdir(inputFolder)):
+
             if file.endswith(".json"):
                 with open(os.path.join(inputFolder, file), 'r') as f:
                     tempData = json.load(f)
+
                     fileData.append(tempData)
 
     except Exception as x:
@@ -53,15 +51,17 @@ def main():
         writer = csv.writer(csvFile)
         keys = ["resourceId", "headline", "description", "articleBody"]
         writer.writerow(keys)
-        itemInfo = []
+       
         
        # write keys from each dictionary in the array to the csv file
         for data in fileData:
+            itemInfo = []
             info = [data[key] for key in keys] 
             if all([isinstance(data[key], StringTypes) for key in keys]):
                 [itemInfo.append(value) for value in info]
             else:
                 itemInfo.append(str(info.decode()))
+
 
 
             writer.writerow(itemInfo)
